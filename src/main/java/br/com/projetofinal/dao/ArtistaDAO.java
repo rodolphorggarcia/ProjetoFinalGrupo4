a72d89d225f18b16b1c079e68e4c9da08071c370
Rodolpho Garcia <rodolphorggarcia@gmail.com>
2020-12-16 18:47:28 -0300

    commit projeto grupo 4

--- src/main/java/br/com/projetofinal/dao/ArtistaDAO.java
+++
@@ -1,9 +0,0 @@
-package br.com.projetofinal.dao;
-
-import org.springframework.data.repository.CrudRepository;
-
-import br.com.projetofinal.beans.Artista;
-
-public interface ArtistaDAO extends CrudRepository<Artista, Integer>{
-
-}
--- src/main/java/br/com/projetofinal/dao/TransactionDAO.java
+++
@@ -1,9 +0,0 @@
-package br.com.projetofinal.dao;
-
-import org.springframework.data.repository.CrudRepository;
-
-import br.com.projetofinal.beans.Transaction;
-
-public interface TransactionDAO extends CrudRepository<Transaction, Integer>{
-
-}
